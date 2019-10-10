75beba55d6fe2429f1bf6bcee397bb0ecff18004
Obaids <obaidalhaddad@gmail.com>
2019-10-10 11:58:54 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,9 @@
 import boto3
+import argparse
+
+parser = argparse.ArgumentParser(description='mode of staging job')
+parser.add_argument('--mode', action="store", dest="mode", default="dev")
+mode = parser.parse_args().mode
 
 print("Hello World!") 
-print("boto3 imported with no problem")
+print("boto3 imported with no problem and added the mode {} successfully ".format(mode))
